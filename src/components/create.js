29a208dd2de72b42bfefd497b1538010f6786716
John Rees <john@bitsushi.com>
2017-02-25 22:30:59 +0000

    dev

--- src/components/create.js
+++ src/components/create.js
@@ -1,19 +1,31 @@
-const flatten = arr => Array.prototype.concat(...arr)
-const a = val => Array.isArray(val) ? val : [val]
+var R = require('ramda')
+var Either = require('ramda-fantasy').Either
+
+const wrap = val => Array.isArray(val) ? val : [val]
+const unwrap = val => val.length === 1 ? val[0] : val
+// const flatten = arr => Array.prototype.concat(...arr)
 // const a = val => Array.isArray(val) ? val : flatten([val])
 
-const create = ({ X = () => (1), Y = () => (1), Z = () => (1) }) => {
-  let point = []
+// const create = ({ X = 1, Y = 1, Z = 1 }) => {
+//   let point = []
 
-  a(X()).forEach(_X => {
-    a(Y()).forEach(_Y => {
-      a(Z()).forEach(_Z => {
-        point.push({ X: _X, Y: _Y, Z: _Z })
-      })
-    })
-  })
+//   wrap(X).forEach(_X =>
+//     wrap(Y).forEach(_Y =>
+//       wrap(Z).forEach(_Z =>
+//         point.push({ X: _X, Y: _Y, Z: _Z })
+//       )
+//     )
+//   )
 
-  return { POINT: (point.length === 1 ? point[0] : point) }
-}
+//   return { POINT: unwrap(point) }
+// }
 
-export default create
+const create = R.curry(({ X = 1, Y = 1, Z = 1 }) => {
+  if (!_.isNumber(X)) return Either.Left(new Error('Input must be numeric'))
+
+  return Either.Right(price + (tax * price)) // <--Wrap result in Either.Right
+})
+
+console.log(create({X: 3}))
+
+// export default create
--- src/components/create.test.js
+++ src/components/create.test.js
@@ -1,13 +1,13 @@
 import create from './create'
 
 it('transparently creates a point', () => {
-  const result = create({X: () => (1), Y: () => (2), Z: () => (3)})
+  const result = create({X: 1, Y: 2, Z: 3})
   const expected = {POINT: {X: 1, Y: 2, Z: 3}}
   expect(result).toEqual(expected)
 })
 
 it('creates points with single array value', () => {
-  const result = create({X: () => ([1, 2, 3]), Y: () => (2), Z: () => (3)})
+  const result = create({X: [1, 2, 3], Y: 2, Z: 3})
   const expected = {
     POINT: [
       {X: 1, Y: 2, Z: 3},
@@ -19,7 +19,7 @@ it('creates points with single array value', () => {
 })
 
 it('creates points with multiple array value', () => {
-  const result = create({X: () => ([1, 2, 3]), Y: () => ([7, 8, 9]), Z: () => (3)})
+  const result = create({X: [1, 2, 3], Y: [7, 8, 9], Z: 3})
   const expected = {
     POINT: [
       {X: 1, Y: 7, Z: 3},
@@ -38,6 +38,6 @@ it('creates points with multiple array value', () => {
   expect(result).toEqual(expected)
 })
 
-// console.log(create({X: () => ([1, 2]), Y: () => (2), Z: () => (3)}))
-// console.log(create({X: () => (1), Y: () => (2), Z: () => (3)}))
-// console.log(create({X: () => ([1, 2, 3]), Y: () => (2), Z: () => (3)}))
+// console.log(create({X: [1, 2], Y: 2, Z: 3}))
+// console.log(create({X: 1, Y: 2, Z: 3}))
+// console.log(create({X: [1, 2, 3], Y: 2, Z: 3}))
